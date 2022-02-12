eee9f919c6df505f5da5017c9f1286cd1a78e0b1
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-02-12 16:09:26 +0100

    image moving inside github graph

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("const columns = Array.from(\r\n  document.querySelectorAll(\".js-calendar-graph > svg > g > g\")\r\n).map((group) => Array.from(group.querySelectorAll(\"rect\")));\r\n\r\nconst img = new Image();\r\nimg.src = \"../bitcoin.jfif\";\r\nimg.onload = () => {\r\n  console.log(img.width);\r\n};\r\n\r\ncolumns.forEach((c, x) =>\r\n  c.forEach((rect, y) => (rect.style.fill = Color(interpolator(0.5)).hex()))\r\n);\r\n\n\n//# sourceURL=webpack://github-contribution-graph/./src/index.js?");
+eval("const columns = Array.from(\r\n  document.querySelectorAll(\".js-calendar-graph > svg > g > g\")\r\n).map((group) => Array.from(group.querySelectorAll(\"rect\")));\r\n\r\nconst img = new Image();\r\n// image pixel's width should be equals to the x squares of github graph (53 square)\r\nimg.src = \"../bitcoin.jfif\";\r\nimg.onload = () => {\r\n  const canvas = document.createElement(\"canvas\");\r\n  const context = canvas.getContext(\"2d\");\r\n  context.drawImage(img, 0, 0);\r\n\r\n  let offSet = 0;\r\n  setInterval(() => {\r\n    columns.forEach((c, x) =>\r\n      c.forEach((rect, y) => {\r\n        const pixel = context.getImageData(x, y + offSet, 1, 1);\r\n        rect.style.fill = `rgba(${pixel.data})`;\r\n      })\r\n    );\r\n\r\n    offSet += 1;\r\n    if (offSet + 7 > img.height) offSet = 0;\r\n  }, 100);\r\n};\r\n\n\n//# sourceURL=webpack://github-contribution-graph/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ const columns = Array.from(
 ).map((group) => Array.from(group.querySelectorAll("rect")));
 
 const img = new Image();
+// image pixel's width should be equals to the x squares of github graph (53 square)
 img.src = "../bitcoin.jfif";
 img.onload = () => {
-  console.log(img.width);
-};
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  context.drawImage(img, 0, 0);
 
-columns.forEach((c, x) =>
-  c.forEach((rect, y) => (rect.style.fill = Color(interpolator(0.5)).hex()))
-);
+  let offSet = 0;
+  setInterval(() => {
+    columns.forEach((c, x) =>
+      c.forEach((rect, y) => {
+        const pixel = context.getImageData(x, y + offSet, 1, 1);
+        rect.style.fill = `rgba(${pixel.data})`;
+      })
+    );
+
+    offSet += 1;
+    if (offSet + 7 > img.height) offSet = 0;
+  }, 100);
+};
